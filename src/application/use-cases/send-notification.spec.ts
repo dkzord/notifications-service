5365344b1931b1e7bb7580ce0c65e6c9d9a248d0
Fernando Cavalcanti <comododk@gmail.com>
2022-12-30 19:11:26 -0300

    test: notification repository

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -1,15 +1,26 @@
+import { Notification } from '../entities/notification/notification';
 import { SendNotification } from './send-notification';
+
+const notifications: Notification[] = [];
+
+const notificationsRepository = {
+  async create(notification: Notification) {
+    notifications.push(notification);
+  },
+};
 
 describe('Send Notification', () => {
   it('should be abel to send a notification', async () => {
-    const sendNotification = new SendNotification();
+    const sendNotification = new SendNotification(notificationsRepository);
 
-    const { notification } = await sendNotification.execute({
+    await sendNotification.execute({
       content: 'This is a notification',
       category: 'social',
       recipientId: '123456',
     });
 
-    expect(notification).toBeTruthy();
+    // console.log(notifications);
+
+    expect(notifications).toHaveLength(1);
   });
 });
