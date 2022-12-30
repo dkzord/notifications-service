05469f15370780c753bfbb5f2318f2c0e63a3508
Fernando Cavalcanti <comododk@gmail.com>
2022-12-30 15:34:58 -0300

    feat: create validator

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -5,6 +6,7 @@ const appPort = process.env.PORT || 3050;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(appPort, () => {
     console.log(`Server running on port http://localhost:${appPort}`);
   });
